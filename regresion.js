799b2ec35ef28a6616f8eb1f03dbb8a468e18d08
Loki <nico2236@gmail.com>
2019-07-18 10:28:12 -0300

    Time measurement and average

--- regresion.js
+++ regresion.js
@@ -28,4 +28,15 @@ return b!=0 ? Math.round((y1 - (b * x1)) / y.length + (b * z)):b;
 }
 
 //Testing [1,2,3,4,5] [5, 5, 5, 6.8, 9], 100 should return 101.
-console.log(`Dado 100 = > ${regression([1,2,3,4,5],[5, 5, 5, 6.8, 9],100)}`);
+let tprom = [0, 0];
+for (let i=0;i<3; i++){
+      let t0 = process.hrtime();
+      console.log(`Dado 100 = > ${regression([1,2,3,4,5],[5, 5, 5, 6.8, 9],100)}`);
+      let t1 = process.hrtime(t0);
+      console.info('Execution time: %ds %dms', t1[0], t1[1] / 1000000)
+      tprom[0]= t1[0];
+      tprom[1]= t1[1];
+}
+tprom[0] = tprom[0]/3;
+tprom[1] = tprom[1]/3;
+console.info('Execution time (average): %ds %dms', tprom[0], tprom[1] / 1000000)
